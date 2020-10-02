656e056404acc3acf67060fe65e88e605abe7cf6
Emerson Almeida <emersonalmeidax@gmail.com>
2020-10-02 09:23:16 -0300

    add singular resoruce

--- lib/juno/resources.rb
+++ lib/juno/resources.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
 require_relative 'resource'
+require_relative 'singular_resource'
 
 %w[transfer].each do |resource|
   Juno.const_set(resource.classify, Class.new(Juno::Resource))
 end
 
-Juno.const_set('Balance'.classify, Class.new(Juno::Resource) do
-  def self.element_path(_id, prefix_options = {}, query_options = nil)
-    check_prefix_options(prefix_options)
-
-    prefix_options, query_options = split_options(prefix_options) if query_options.nil?
-    "#{prefix(prefix_options)}#{element_name}#{format_extension}#{query_string(query_options)}"
-  end
-end)
+%w[balance].each do |resource|
+  Juno.const_set(resource.classify, Class.new(Juno::SingularResource))
+end
---
+++ lib/juno/singular_resource.rb
@@ -0,0 +1,10 @@
+module Juno
+  class SingularResource < Juno::Resource
+    def self.element_path(_id, prefix_options = {}, query_options = nil)
+      check_prefix_options(prefix_options)
+
+      prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+      "#{prefix(prefix_options)}#{element_name}#{format_extension}#{query_string(query_options)}"
+    end
+  end
+end
