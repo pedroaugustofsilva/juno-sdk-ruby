64ced5502908b2451dc574c86a559dd7ee78da82
Pedro Augusto <pedro@nullbug.dev>
2021-07-22 20:22:58 -0300

    feat(charges): removed transaction module

--- lib/juno.rb
+++ lib/juno.rb
@@ -3,7 +3,7 @@
 module Juno
   autoload :Authorization, 'juno/authorization.rb'
   autoload :Configuration, 'juno/configuration.rb'
-  autoload :Charges, 'juno/transaction/charges.rb'
+  autoload :Charges, 'juno/charges.rb'
   autoload :Webhooks, 'juno/webhooks.rb'
   autoload :Resource, 'juno/resource.rb'
 
--- lib/juno/configuration.rb
+++ lib/juno/configuration.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-#
+
 module Juno
   class Configuration
     POSSIBLES_ENVIRONMENTS = %i[sandbox production].freeze
